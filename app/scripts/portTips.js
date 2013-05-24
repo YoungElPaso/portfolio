442712b62b5688c024b57268415b1ff667ab6e30
Jesse Sutherland <jesse.sutherland@mcgill.ca>
2013-05-24 16:22:17 -0400

    added portfolio tips js file (portTips.js) included into main.js (require.js) method, and updated Readme.md

---
+++ app/scripts/portTips.js
@@ -0,0 +1,7 @@
+/*global define */
+define(['jquery', 'bootstrap'], function () {
+	'use strict';
+
+	//Init some BS tooltips for some nice enhancements. Options are inline.
+	$('#about-site').tooltip();
+});
