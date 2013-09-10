0865bdd631fc9410502a31062067cc6f5eae6d09
Charlene and Jesse <jesse@ubuntu.(none)>
2013-09-10 00:28:19 -0400

    added yepnope to project through bower, modified index.html content, added yepnope and modernizr test for mobile, and loading desktop.css if not mobile

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,27 @@
 /*global define */
-define([], function () {
-	'use strict';
+define([], function() {
+    'use strict';
 
-	return '\'Allo \'Allo!';
+    Modernizr.addTest('is_mobile', function() {
+        var t1 = Modernizr.touch;
+        var t2 = Modernizr.mq('only all and (min-width: 480px)');
+        if (t1 == false && t2 == false) {
+            return false;
+        } else {
+            return true;
+        }
+    })
+
+    /**
+     * Use yepnope to test touch / resolution and load extra styles.
+     */
+    yepnope({
+        test: Modernizr.is_mobile,
+        nope: {
+            'desktopExtras': 'styles/desktop.css'
+        },
+        callback: function(url, result, key) {
+            $('body').addClass('desktop');
+        }
+    });
 });
