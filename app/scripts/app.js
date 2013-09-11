685d2bb67c425a007f45f22fd612f402fb051c1d
Charlene and Jesse <jesse@ubuntu.(none)>
2013-09-11 00:42:13 -0400

    cleaned up index for templating, removed test content.  refactored app a bit and added mobileonly css and js calls. forgot to add index and app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,10 +5,10 @@ define([], function() {
     Modernizr.addTest('is_mobile', function() {
         var t1 = Modernizr.touch;
         var t2 = Modernizr.mq('only all and (max-width: 480px)');
-        if (t1 == false && t2 == false) {
-            return false;
-        } else {
+        if (t1 == true || t2 == true) {
             return true;
+        } else {
+            return false;
         }
     })
 
@@ -17,11 +17,21 @@ define([], function() {
      */
     yepnope({
         test: Modernizr.is_mobile,
+        yep: {
+            'mobileOnly': 'styles/mobile-only.css'
+        },
         nope: {
             'desktopExtras': 'styles/desktop.css'
         },
         callback: function(url, result, key) {
-            $('body').addClass('desktop');
+            switch (key) {
+                case 'mobileOnly':
+                    $('body').addClass('mobile');
+                    break;
+                case 'desktopExtras':
+                    $('body').addClass('desktop');
+                    break;
+            };
         }
     });
 });
